520eb7ca3dd6f804a80a53bd78376577237f0e02
RidwanAdebosin <adebosinadewale@gmail.com>
2024-08-09 11:05:16 +0100

    About folder created

--- src/components/About/Contact/Contact.tsx
+++ src/components/About/Contact/Contact.tsx
@@ -1,12 +1,16 @@
 import "./Contact.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 const Contact = () => {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
   return (
     <main>
-      <section className="contact-hero">
-        <div className="contact-hero-wrapper">
+      <section className="contact-hero-wrapper">
+        <div data-aos="fade-down" className="contact-hero">
           <h1>Contact Us</h1>
           <p>
             Whether you have a query, need more information, or are ready to
@@ -16,26 +20,28 @@ const Contact = () => {
       </section>
       <section className="contact-details">
         <div className="contact-box">
-          <img data-aos="fade-down" src="" alt="message logo" />
-          <p>eze@gmail.com</p>
-          <small>Email us your enquiries</small>
+          <img data-aos="zoom-in" src="" alt="message logo" />
+          <p data-aos="zoom-in">eze@gmail.com</p>
+          <small data-aos="zoom-in">Email us your enquiries</small>
         </div>
         <div className="contact-box">
-          <img src="" alt="phone icon" />
-          <p>+234 999 999 9999</p>
-          <small>Send us a message on WhatsApp</small>
+          <img data-aos="zoom-in" src="" alt="phone icon" />
+          <p data-aos="zoom-in">+234 999 999 9999</p>
+          <small data-aos="zoom-in">Send us a message on WhatsApp</small>
         </div>
       </section>
       <section className="contact-extras">
         <section className="contact-extra">
-          <p>Get in Touch</p>
-          <h1>Need more info? Contact us. We're happy to help</h1>
-          <p>
+          <p data-aos="fade-down">Get in Touch</p>
+          <h1 data-aos="fade-up">
+            Need more info? Contact us. We're happy to help
+          </h1>
+          <p data-aos="fade-up">
             Need more information? Reach us on all our channels, and we'll be
             happy to respond.{" "}
           </p>
         </section>
-        <section className="contact-image">
+        <section data-aos="fade-left" className="contact-image">
           <img
             src="https://www.pressdng.com/Pressd%20laundry%20support%20image.jpg"
             alt="a girl looking into a washing machine"
